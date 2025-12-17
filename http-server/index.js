764487eaec2e1cba4d6c69a4ae4bf9a07bb040cb
snehzx <24CSEAIML075@jssaten.ac.in>
2025-12-18 02:00:20 +0530

    making of http-server

---
+++ http-server/index.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+  const log = `${Date.now()}: ${req.method}:${req.url} :new req received\n`;
+  const myUrl = url.parse(req.url, true);
+  // req.url gives a string and url.parse parses that string into object
+  // true tells to parse the query string into object
+
+  fs.appendFile("log.txt", log, (err, data) => {
+    if (err) {
+      console.error("LOG ERROR:", err);
+      res.end("internal error");
+      return;
+    } else {
+      switch (myUrl.pathname) {
+        case "/":
+          if (req.method === "GET") res.end("home page");
+          break;
+        case "/about":
+          const search = myUrl.query.search_query;
+          res.end("here are your results for " + search);
+          break;
+        case "/login":
+          res.end("you need to login in first");
+          break;
+        default:
+          res.end("404 not found");
+          break;
+      }
+    }
+  });
+});
+server.listen(8000, () => console.log("server started"));
